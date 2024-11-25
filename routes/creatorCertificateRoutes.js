10ed156812b8d98816a0f24cd6424fcb86c86db6
Anders <andersraae01@gmail.com>
2024-11-25 15:06:24 +0100

    groupTree 114 WIP

--- routes/creatorCertificateRoutes.js
+++ routes/creatorCertificateRoutes.js
@@ -7,6 +7,9 @@ const checkIds = require('../middlewares/checkIds');
 const errorCodes = require('../helpers/errorCodes');
 const axios = require('axios');
 
+const { generateStudentPDF } = require('../helpers/pdfGenerator');
+const { getStudentHtml } = require('../helpers/templates');
+
 const { BACKEND_URL } = require('../config/keys');
 
 
@@ -51,7 +54,8 @@ router.get('/creator/:id', async (req, res) => {
 		const creatorId = new mongoose.Types.ObjectId(req.params.id);
 
 		// Find creator-certificate by creatorId and courseId
-		let certificates = await CreatorCertificateModel.find({ creatorId: creatorId });
+		console.log(creatorId._id);
+		let certificates = await CreatorCertificateModel.find({ creatorId: creatorId._id });
 
 		if (certificates.length === 0) {
 			return res.status(204).send();
@@ -97,6 +101,63 @@ router.delete('/', checkIds, async (req, res) => {
 		return res.status(500).json({ error: errorCodes('CE0001') });
 	}
 });
+
+
+// Generate and download student-certificate
+router.post('/download', async (req, res) => {
+	let { creatorId, courseId } = req.query;
+
+	console.log('9fdsssssss', creatorId, courseId);
+  
+	try {
+	  creatorId = new mongoose.Types.ObjectId(creatorId);
+	  courseId = new mongoose.Types.ObjectId(courseId);
+	} catch (error) {
+	  return res.status(400).json({ error: errorCodes('CE0101', 'creatorId and courseId') });
+	}
+  
+	let certificate;
+  
+	try {
+	  certificate = await CreatorCertificateModel.findOne({ creatorId: creatorId, courseId: courseId });
+	} catch (error) {
+	  console.log(error);
+	  return res.status(500).json({ error: errorCodes('CE0001') });
+	}
+  
+	if (!certificate) {
+	  console.log('CERT:', certificate);
+	  return res.status(404).send({ error: errorCodes('CE0002', 'Certificate')}); 
+	}
+  
+	const pdfPath = await generateStudentPDF({
+	  studentName: 'navn',
+	  creatorName: 'dfghjk',
+	  courseName: 'certificate.courseName',
+	  dateOfCompletion: new Date("2023-11-10T22:00:00.000+00:00"),
+	  estimatedCourseDuration: '6',
+	}, creatorId, courseId);
+
+	console.log(pdfPath);
+  
+	return res.status(200).sendFile(pdfPath);
+  });
+
+  router.get('/preview', async (req, res) => {
+	const returnVal = await getStudentHtml({
+		studentName: 'navn',
+		creatorName: 'dfghjk',
+		courseName: 'certificate.courseName',
+		dateOfCompletion: new Date("2023-11-10T22:00:00.000+00:00"),
+		estimatedCourseDuration: '6',
+		});
+
+		console.log(returnVal);
+
+	
+	return res.status(200).send(returnVal);
+  });
+  
 
 async function makeCertificates(certificateList, token, creator) {
 	let certificates = [];
